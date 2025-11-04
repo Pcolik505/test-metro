c0359084475db106028ba063142f4b8c5b29ca44
G-i-x <k.f.b.ekb@gmail.com>
2025-11-04 19:39:37 +0500

    Revert "Merge pull request #7 from AtlasSpa/master"
    This reverts commit 552db87379887e12c93e76c2ac97df8039ff5104, reversing
    changes made to d5f9b8512b9d9d4274ea7cdd264bf36a77aadae3.

--- Content.Shared/_S/Adrenaline/AdrenalineComponent.cs
+++
@@ -1,7 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared.Adrenaline;
-
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class AdrenalineStatusComponent : Component { }
